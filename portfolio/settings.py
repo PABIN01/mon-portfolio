0a4fd3a806a66e471ad3717992f606457b166fbb
Michael Pabingui <pabinmichaelb12@gmail.com>
2026-08-01 14:39:05 +0100

    Fix: CompressedStaticFilesStorage sans manifest

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -130,6 +130,6 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
